2e6f34e18ba0112e806d43c4c80fdf4e69764d64
Christiaan Hougaard <cwhougaard@gmail.com>
2021-06-19 15:24:07 -0400

    cleaned public folder and added body classes to index.html

--- frontend/src/components/shared/Navigation.js
+++ frontend/src/components/shared/Navigation.js
@@ -4,16 +4,14 @@ import { NavLink as RouteLink } from 'react-router-dom'
 
 const Navigation = () => {
     return (
-        <navbar className="grid-nav">
-            <nav class="grid-nav">
-                <ul class="nav-bar">
-                    <li><a href="/">Home</a></li>
-                    <li><a href="/resume">Resume</a></li>
-                    <li><a href="/portfolio">Portfolio</a></li>
-                    <li><a href="/contact">Contact Us</a></li>
-                </ul>
-            </nav>
-        </navbar>
+    <nav class="grid-nav">
+        <ul class="nav-bar">
+            <li><a href="/">Home</a></li>
+            <li><a href="/resume">Resume</a></li>
+            <li><a href="/portfolio">Portfolio</a></li>
+            <li><a href="/contact">Contact Us</a></li>
+        </ul>
+    </nav>
     )
 }
 
