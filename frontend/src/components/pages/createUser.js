187d6148e4cf7155b911c9335539d3efa4b3b1e0
Christiaan Hougaard <cwhougaard@gmail.com>
2021-06-21 20:16:38 -0400

    added error message to create user and styled form a bit

--- frontend/src/components/pages/createUser.js
+++ frontend/src/components/pages/createUser.js
@@ -1,8 +1,12 @@
 import React, { useState } from 'react'
+import { useHistory, useLocation } from 'react-router-dom'
 import Logo from "../shared/logo"
 import parseJwt from '../../helpers/authHelper'
 
 const CreateUser = () => {
+    let history = useHistory();
+    let location = useLocation();
+    const [error, setError] = useState(true)
     const [name, setUsername] = useState("")
     const [password, setPassword] = useState("")
     const [email, setEmail] = useState("")
@@ -25,22 +29,28 @@ const CreateUser = () => {
         }, [token])
         const payload = await response.json()
         if (response.status >= 400) {
-            
+            setError(false)
         } else {
-            alert("User created successfully!")
+            alert("User created successfully! Please Log In")
+
+            let { from } = location.state || { from: { pathname: "/submissions" } };
+            history.replace(from);
         }
     }
     return (
       <>
       <Logo />
         <form onSubmit={userSubmit}>
+        {!error && 
+            <p className="error-message grid-title-resume flex">Unable to create user! Password must be a minimum of 8 characters and you need a valid email address.</p>
+        }
             <label htmlFor="usernameEntry">Username</label>
                 <input type="text" name="name" id="usernameEntry" placeholder="Username" value={name} onChange={e => setUsername(e.target.value)}/>
             <label htmlFor="emailEntry">Email</label>
                 <input type="email" name="email" id="emailEntry" placeholder="Email" value={email} onChange={e => setEmail(e.target.value)}/>
             <label htmlFor="passwordEntry">Password</label>
                 <input type="password" name="password" id="passwordEntry" placeholder="Valid password" value={password} onChange={e => setPassword(e.target.value)}/>
-            <button>Create User</button>
+            <button className="form-btn">Create User</button>
         </form>
       </>
     )
